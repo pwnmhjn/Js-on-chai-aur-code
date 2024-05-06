8cd432c703d63479777b226dc37c3da921351ad9
Pawan Mahajan <148587577+pwnmhjn@users.noreply.github.com>
2024-05-06 06:14:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "pwnmhjn@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore" 
+let accountState;
+// accountId = 2
+accountEmail = "pawan@gmail.com"
+accountPassword = "12345"
+accountCity = "Pansemal"
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// alert("hello") we are using nodeJS not browser
+
+console.log(3
+    +3)  //code readability should be high
+console.log("Pawan")
+
+let name = "pwnmhjn"
+let age = 25
+let isLoggedIn = true
+
+ console.log(typeof(undefined))
