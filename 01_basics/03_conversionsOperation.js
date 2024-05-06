5f6b6949f97aa8e1a273a8debbe0bdc1be255f48
Pawan Mahajan <148587577+pwnmhjn@users.noreply.github.com>
2024-05-06 06:30:09 +0000

    conversion of data types

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,17 @@
+// let score = "true"
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console .log(typeof(valueInNumber))
+// console .log(valueInNumber)
+
+// // '33' => 33
+// "34sd" = > NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+console.log(typeof(booleanIsLoggedIN))
+console.log(booleanIsLoggedIN)
